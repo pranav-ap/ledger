0ea799086c42445055aca81ead637336308e1102
pranav-ap <pranavap96@gmail.com>
2018-10-29 21:07:14 +0530

    expenses bar is setup

--- client/src/components/HomeInput.js
+++ client/src/components/HomeInput.js
@@ -1,16 +1,60 @@
 import React, { Component } from 'react';
 
 class HomeInput extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      item: '',
+      cost: 0
+    }
+  }
+
+  handleOnEnter(e) {
+    e.preventDefault()
+
+    let inputbar = document.getElementById('inputbar')
+    let text = inputbar.value
+
+    if (text.length === 0 || e.keyCode !== 13) {
+      return
+    }
+
+    if (this.state.item === '') {
+      this.setState({ 'item': text })
+      inputbar.placeholder = 'How much did it cost ?'
+      inputbar.value = ''
+    }
+    else if (isNaN(text)) {
+      inputbar.value = ''
+      inputbar.placeholder = 'Please enter a number'
+    }
+    else {
+      this.setState({ 'cost': Number(text) }, () => {
+        inputbar.value = ''
+        inputbar.placeholder = 'What did you buy ?'
+      })
+    }
+  }
+
   render() {
     return (
       <div className='HomeInput'>
         <div className='field'>
           <div className='control'>
-            <input className="input is-rounded is-large custom-input" type="text" placeholder="What did you buy ?" />
+            <input
+              id='inputbar'
+              ref='inputbar'
+              className="input is-rounded is-large custom-input"
+              type="text"
+              placeholder="What did you buy ?"
+              onKeyUp={(e) => this.handleOnEnter(e)}
+              autoFocus />
           </div>
         </div>
         <div id='Subtext'>
-          <span>Cancel</span>
+          <span id='Item'></span>
+          <span id='Cancel'>Cancel</span>
         </div>
       </div>
     );
