89570be29968c1acb12df8379fadfb9705120eaf
Pranav <pranavap96@gmail.com>
2019-02-07 20:18:21 +0530

    update

--- client/src/components/CashTable.js
+++ client/src/components/CashTable.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
-import { startDeleteTransaction } from './../actions/transactions-actions'
+import { startUpdateTransaction, startDeleteTransaction } from './../actions/transactions-actions'
 
 class CashTable extends Component {
   renderRows() {
@@ -19,12 +19,25 @@ class CashTable extends Component {
       )
     }
 
-    const handleEdit = (e) => {
-      console.log(e)
-    }
+    const handleEdit = (transaction) => {
+      let row = document.getElementById(transaction.id)
 
-    const handleSave = (e) => {
-      console.log(e)
+      row.cells[1].contentEditable = !row.cells[1].contentEditable
+      row.cells[2].contentEditable = !row.cells[2].contentEditable
+
+      if (row.cells[1].contentEditable === false) {
+        transaction = {
+          ...transaction,
+          item: row.cells[1].innerText,
+          expense: row.cells[2].innerText
+        }
+
+        dispatch(startUpdateTransaction(transaction.id, transaction))
+      }
+
+      if (row.cells[1].contentEditable === true) {
+        row.cells[1].focus()
+      }
     }
 
     let count = 0
@@ -33,16 +46,16 @@ class CashTable extends Component {
       count++
 
       return (
-        <tr key={count}>
+        <tr key={count} id={transaction.id}>
           <th>{count}</th>
-          <td>{transaction.item}</td>
-          <td>{transaction.expense}</td>
+          <td contentEditable={false}>{transaction.item}</td>
+          <td contentEditable={false}>{transaction.expense}</td>
           <td
             className='row-btn'
             onClick={() => dispatch(startDeleteTransaction(transaction.id))}><i className='far fa-trash-alt delete-btn'/></td>
           <td
             className='row-btn'
-            onClick={(e) => handleEdit(e)}><i className='far fa-edit edit-btn'/></td>
+            onClick={() => handleEdit(transaction)}><i className='far fa-edit edit-btn'/></td>
         </tr>
       )
     })
